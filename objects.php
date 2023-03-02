ca1b21076315bf9ecf2a6128386f41e88da86f51
kalbers <ken.albers@gmail.com>
2023-03-02 13:44:28 -0500

    Home updates

--- objects.php
+++ objects.php
@@ -9,12 +9,16 @@
       </div>
 
       <div class="medium-12 cell">
-        <p>Some intro text. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae voluptas natus! Sit, atque distinctio harum facilis vitae, quos sed fuga dignissimos ducimus praesentium laboriosam est. Quidem doloremque saepe sapiente! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam tempore commodi blanditiis ratione nesciunt mollitia officiis harum et libero incidunt sapiente, architecto iure deleniti ex suscipit laborum omnis nisi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aperiam repellat placeat id distinctio reiciendis amet quod voluptate temporibus! Aliquid magni placeat corporis optio exercitationem eligendi unde voluptates? Cupiditate, iusto?</p>
+        <p>Some intro text. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quae voluptas natus! Sit, atque distinctio harum facilis vitae, quos sed fuga dignissimos ducimus praesentium laboriosam est. Quidem doloremque saepe sapiente! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam tempore commodi blanditiis ratione nesciunt mollitia officiis harum et libero incidunt sapiente.</p>
       </div>
 
       <hr>
 
       <div class="grid-x grid-padding-x">
+      <div class="medium-12 cell">
+        <h3>Item Sets</h3>
+        <p>Orienting text. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo deleniti alias natus rem ipsum ex expedita optio vero quisquam enim, corrupti minus laudantium.</p>
+        </div>
         <div class="medium-4 cell"><a href="objects-item-set.php"><img src="https://via.placeholder.com/400x250.png?text=ITEM+SET+(THEME)" alt=""></a></div>
         <div class="medium-4 cell"><a href="objects-item-set.php"><img src="https://via.placeholder.com/400x250.png?text=ITEM+SET+(THEME)" alt=""></a></div>
         <div class="medium-4 cell"><a href="objects-item-set.php"><img src="https://via.placeholder.com/400x250.png?text=ITEM+SET+(THEME)" alt=""></a></div>
@@ -23,6 +27,10 @@
       <hr>
 
       <div class="grid-x grid-padding-x">
+      <div class="medium-12 cell">
+      <h3>Featured Items</h3>
+        <p>Orienting text. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo deleniti alias natus rem ipsum ex expedita optio vero quisquam enim, corrupti minus laudantium.</p>
+        </div>
         <div class="medium-3 cell"><a href="objects-item.php"><img src="https://via.placeholder.com/400x250.png?text=ITEM" alt=""></a></div>
         <div class="medium-3 cell"><a href="objects-item.php"><img src="https://via.placeholder.com/400x250.png?text=ITEM" alt=""></a></div>
         <div class="medium-3 cell"><a href="objects-item.php"><img src="https://via.placeholder.com/400x250.png?text=ITEM" alt=""></a></div>
